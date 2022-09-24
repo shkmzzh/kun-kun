9e77ef51d58a2615438162467930325b65de4e43
shkmzzh <3081764857@qq.com>
2022-09-24 13:38:26 +0800

    功能优化

--- game.js
+++ game.js
@@ -307,7 +307,7 @@ btn.addEventListener('click', function (e) {
     btnHide()
   }
   if (e.target.innerHTML === '困难模式') {
-    localStorage.setItem('ikun', '15')
+    localStorage.setItem('ikun', '18')
     btnHide()
   }
   if (e.target.innerHTML === '地狱模式') {
